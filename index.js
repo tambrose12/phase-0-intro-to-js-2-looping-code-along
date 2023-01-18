2b6efa4ecad06cbac124a776cefa14b98b07751e
Taylor Ambrose <taykesby@gmail.com>
2023-01-17 21:38:09 -0500

    commit tree c19dc53394386545bd9bcc90e34f897d65d4dcf4

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solutions in this file
+
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+// const guests = ["Guadalupe", "Ollie", "Aki"];
+
+
+
+function writeCards(list, wordToInsert) {
+
+    const output = [];
+
+    for (let i = 0; i < list.length; i++) {
+
+        const message = `Thank you, ${list[i]}, for the wonderful ${wordToInsert} gift!`;
+
+        output.push(message);
+    }
+
+    return output;
+}
+
+
+function countDown(int) {
+    for (let i = int; i >= 0; i--) {
+        console.log(i)
+    }
+
+}
+
+
